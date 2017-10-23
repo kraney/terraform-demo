074789184c1b1aabdb560fa45a8e8fc77f06756d
Kris Raney <kraney@ixiacom.com>
2017-10-23 11:41:21 -0700

    Try using variables

--- ntopng.tf
+++ ntopng.tf
@@ -1,3 +1,26 @@
+variable "cluster_host" {
+  type = "string"
+}
+variable "cluster_username" {
+  type = "string"
+}
+variable "cluster_password" {
+  type = "string"
+}
+
+
+provider "helm" {
+    kubernetes {
+        host     = "${var.cluster_host}"
+        username = "${var.cluster_username}"
+        password = "${var.cluster_password}"
+
+        client_certificate     = "${file("~/.kube/client-cert.pem")}"
+        client_key             = "${file("~/.kube/client-key.pem")}"
+        cluster_ca_certificate = "${file("~/.kube/cluster-ca-cert.pem")}"
+    }
+}
+
 resource "helm_chart" "ntopng" {
   name = "terraform-ntop"
   chart = "/Users/kraney/Documents/workspace/netservice/netservices/ntopng-0.1.0.tgz"
